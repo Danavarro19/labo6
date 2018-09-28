3caa22e97a10125691629963daa54dd2f3a76ae3
Daniel <00110017@uca.edu.sv>
2018-09-28 11:33:30 -0600

    commit

---
+++ Ejercicio_10.js
@@ -0,0 +1,11 @@
+function crearUsr(nombre, apellido,fechaNacim,numero,correo){
+    let usr = {
+        nombre,
+        apellido,
+        fechaNacim,
+        numero,
+        correo,
+    }
+    console.log(usr);
+    return usr;
+}
---
+++ Ejercicio_11.js
@@ -0,0 +1,11 @@
+var users = [];
+function agregarUsr(nombre, apellido,fechaNacim,numero,correo){
+    user = {
+        nombre,
+        apellido,
+        fechaNacim,
+        numero,
+        correo,
+    }
+    users.push(user);
+}
