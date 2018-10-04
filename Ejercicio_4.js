dfcaaa7735578df8d1ca2fe0a9aec4a7937afc61
Danavarro19 <37382359+Danavarro19@users.noreply.github.com>
2018-10-03 18:08:12 -0600

    Update Ejercicio_4.js

--- Ejercicio_4.js
+++ Ejercicio_4.js
@@ -1,14 +1,10 @@
-
-let arreglo = new Array(1,2,3,4,5,6,7,8,9,10,11);
-
-function despliegue(arreglo){
-    for(let i=0;i<=arreglo.length;i++){
-        console.log(arreglo[0] + arreglo[arreglo.length - 1]);
-        arreglo.pop();
-        arreglo.shift();
-        
-        if(arreglo.length == 1){
-            console.log(arreglo[0] + arreglo[0]);
-        }
-    }
+function sumEdges(array){
+  let n = array.length;
+      a = array.splice(Math.trunc(n/2)).reverse();
+      b = array.reduce((result, number, index) => {
+              result[index] = number+a[index];
+              return result;
+            }, []);
+      n % 2 == 0 ?  b : b.push(a[a.length-1]*2);
+      return b;
 }
