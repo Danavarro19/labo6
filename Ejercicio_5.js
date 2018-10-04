08447695bd5fd878ff356cbcfc317c9917a763c8
Danavarro19 <37382359+Danavarro19@users.noreply.github.com>
2018-10-03 18:08:38 -0600

    Update Ejercicio_5.js

--- Ejercicio_5.js
+++ Ejercicio_5.js
@@ -1,13 +1,1 @@
-let lista = new Array(1,2,3,4,"hola","adios");
-let tipo = "string";
-
-function ArregloTipos(lista,tipo){
-    let total = 0;
-    for(i in lista){
-        if(tipo == typeof lista[i]){
-            total += 1;
-        }
-    }
-    console.log(tipo+": "+total);
-}
-
+let returnOneType = (array, type) => array.filter(element => typeof(element) == type);
