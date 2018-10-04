2417fcb11e2a37d96e6289dfb2357a0f20a9dd06
Danavarro19 <37382359+Danavarro19@users.noreply.github.com>
2018-10-03 18:06:02 -0600

    Update Ejercicio_2.js

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,10 +1,5 @@
-function suma(arreglo){
-    var contador = 0;
-    var sum = 0;
-    for(let i of arreglo){
-        contador = contador + 1;
-        sum = sum +i;
-    }
-    console.log("Suma:" + sum + " Promedio:" + (sum/contador));
+function suma_promedio(array){
+  let a =  array.reduce((total, number) => total + number),
+      b = a/array.length;
+  return [a,b.toFixed(2)];
 }
-
