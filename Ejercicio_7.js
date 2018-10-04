493a820abaa7cf803e4e4d7c30d67186dfa70062
Danavarro19 <37382359+Danavarro19@users.noreply.github.com>
2018-10-03 18:09:51 -0600

    Update Ejercicio_7.js

--- Ejercicio_7.js
+++ Ejercicio_7.js
@@ -1,16 +1,7 @@
 function incerteza([...numbers]){
-    let n = numbers.length;
-    let sum = 0;
-    for (let i of numbers){
-        sum = sum + i;
-    }
-    let x = sum/n;
-    let sum2 = 0;
-    for (let j =0 ; j < n;j++){
-        sum2 = sum2 + Math.pow((numbers[j]-x),2);
-    }
-    let inc = (1/(n-1))*sum2;
-    return String(x.toFixed(1))+" +- "+String(Math.sqrt(inc).toFixed(1));
+  let n = numbers.length,
+      x = numbers.reduce((total,number) => total+number,0)/n;
+      y= numbers.map(number => Math.pow(number-x,2))
+                .reduce((total,number) => total+number, 0);
+  return `incerteza: ${x.toFixed(2)}(+-)${(1/(n-1)*y).toFixed(2)}`;
 }
-
-
